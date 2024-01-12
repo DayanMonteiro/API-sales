c0cd7d643aa05d7135bf5bc93ccc7d68c3f7ae81
DayanMonteiro <dmsmamb@gmail.com>
2024-01-12 17:06:38 -0300

    create and list products service

---
+++ src/modules/products/services/CreateProductsService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import AppError from '@shared/errors/AppError';
+import Product from '../typeorm/entities/Product';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProdutcService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is already one product with this name');
+    }
+
+    // prepares the product to be created
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    // create product
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProdutcService;
