19b98e28081692812673a9d08fe3b96a33339cbd
DayanMonteiro <dmsmamb@gmail.com>
2024-01-18 16:47:16 -0300

    multer lib configuration for upload

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import AppError from '@shared/errors/AppError';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
+interface ITokenPaylod {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -20,6 +26,13 @@ export default function isAuthenticated(
 
   try {
     const decodeToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodeToken as ITokenPaylod;
+
+    // pra esse user não quebrar tem a gambeta feita src/@types/express/index.d.ts
+    request.user = {
+      id: sub,
+    };
 
     return next();
   } catch {
