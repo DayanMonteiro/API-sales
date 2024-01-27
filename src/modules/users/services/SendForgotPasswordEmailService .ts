6899df747be022ef0e055d3b8e07988d8bfd1e97
DayanMonteiro <dmsmamb@gmail.com>
2024-01-27 13:44:35 -0300

    ethereial fake email service

--- src/modules/users/services/SendForgotPasswordEmailService .ts
+++ src/modules/users/services/SendForgotPasswordEmailService .ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository ';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -23,6 +24,11 @@ class SendForgotPasswordEmailService {
     const token = await userTokensRepository.generate(user.id);
 
     console.log('token', token);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+    });
   }
 }
 
